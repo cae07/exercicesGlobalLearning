e47a04cb442df3ea1907f269a2268198342d544c
cae <eduardokae01@hotmail.com>
2023-03-26 13:31:08 -0300

    feat: commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// && -> AND
+// || -> OR
+// ! -> NOT
+
+const firstCondition = true;
+const secondCondition = false;
+
+// const isHollidays = firstCondition && secondCondition;
+// console.log(isHollidays);
+
+// const isTrue = firstCondition || secondCondition;
+// console.log(isTrue);
+
+const isFalse = !firstCondition;
+console.log(isFalse);
