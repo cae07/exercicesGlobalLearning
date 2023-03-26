f40daba9c8f6cef3fe0660945603ebcc9154f25b
cae <eduardokae01@hotmail.com>
2023-03-26 12:07:46 -0300

    feat: class

---
+++ operacoes.js
@@ -0,0 +1,17 @@
+// + - * / **
+
+let salary = 2500;
+
+// console.log(salary + salary);
+// console.log(salary - salary);
+// console.log(salary * salary);
+// console.log(salary / salary);
+// console.log(3 ** 3);
+// salary ++
+// console.log(salary);
+// salary --
+// console.log(salary);
+
+let count = 0;
+
+console.log('first => ', count -= 1);
