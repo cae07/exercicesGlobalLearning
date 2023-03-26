2fedb3d5d479efba8203a24fe017db90665144b4
cae <eduardokae01@hotmail.com>
2023-03-26 11:43:07 -0300

    feat: types class

---
+++ tiposPrimitivos.js
@@ -0,0 +1,14 @@
+let age = 42; // number
+let firstName = "Caê"; // string
+let isTrue = true; // boolean
+
+let array = [];
+
+let object = {
+  firstName,
+  age,
+  isTrue
+}
+
+console.log("typeof object =", typeof object);
+console.log("typeof array =", typeof array);
