3f73b4246a93ee6f9666a8c78c2ce63c81c07f2b
cae <eduardokae01@hotmail.com>
2023-03-26 11:28:54 -0300

    feat: variable class

---
+++ aulaVariavel.js
@@ -0,0 +1,5 @@
+var firstVariable = "Primeira variavel"; // mutavel -> hoisting
+let secondVariable = "Segunda variavel"; // mutavel
+const thirdVariable = "Terceira variavel"; // imutavel
+
+thirdVariable = true;
